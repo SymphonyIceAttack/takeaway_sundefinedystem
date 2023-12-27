07b2668b4712ae05ca667133962354206902bfde
蔡震 <1994370030@qq.com>
2023-12-27 16:11:00 +0800

    workspace init

--- app/(UserPage)/landing/page.tsx
+++ app/(UserPage)/landing/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const page = () => {
-    return <div>page</div>
+    return <div className="p-1 m-1 bg-slate-500"></div>
 }
 
 export default page
---
+++ app/(UserPage)/layout.tsx
@@ -0,0 +1,64 @@
+'use client'
+import { LuPizza } from 'react-icons/lu'
+
+import React, { PropsWithChildren, useState } from 'react'
+import {
+    Navbar,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    Button,
+} from '@nextui-org/react'
+import { usePathname } from 'next/navigation'
+const layout = ({ children }: PropsWithChildren<{}>) => {
+    const pathname = usePathname()
+
+    return (
+        <div>
+            <Navbar maxWidth="full">
+                <NavbarContent className="">
+                    <LuPizza size={30} />
+                </NavbarContent>
+                <NavbarContent className="gap-4" justify="end">
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Features
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive>
+                        <Link href="#" aria-current="page">
+                            Customers
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Integrations
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+                {pathname === '/landing' && (
+                    <NavbarContent
+                        className="mx-2"
+                        justify="end"
+                        style={{ flexGrow: 0 }}>
+                        <NavbarItem className="">
+                            <Link href="#">登录</Link>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Button
+                                as={Link}
+                                color="primary"
+                                href="#"
+                                variant="flat">
+                                注册
+                            </Button>
+                        </NavbarItem>
+                    </NavbarContent>
+                )}
+            </Navbar>
+            {children}
+        </div>
+    )
+}
+
+export default layout
